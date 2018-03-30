787c2c45e663b17f69718a566e526322a26e719c
Diogo Vianna V. Araújo <diogoviannaaraujo@gmail.com>
2018-03-30 00:01:27 -0300

    Added basic site structure

---
+++ components/base/Body.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Head from 'next/head'
+
+const Index = ({ children }) => (
+  <div>
+    <Head>
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <meta charSet='utf-8' />
+    </Head>
+    <style jsx global>{`
+      html {
+        height: 100%;
+      }
+      body {
+        margin: 0;
+        background-image: url('/static/bg.jpg');
+        background-size: cover;
+        background-position: center center;
+        background-repeat: no-repeat;
+        height: 100%;
+      }
+    `}</style>
+    { children }
+  </div>
+)
+
+Index.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]).isRequired
+}
+
+export default Index
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Body from '~/components/base/Body'
+
+export default () => (
+  <Body>
+    <span> brisa </span>
+  </Body>
+)
