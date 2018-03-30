a191a6d8a8f0cd8acd3699dd77d785279f445897
Diogo Vianna V. Araújo <diogoviannaaraujo@gmail.com>
2018-03-30 00:34:41 -0300

    Added basic initial home

--- components/base/Body.js
+++ components/base/Body.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Head from 'next/head'
 
 const Index = ({ children }) => (
-  <div>
+  <div style={{height: '100%'}}>
     <Head>
       <meta name='viewport' content='width=device-width, initial-scale=1' />
       <meta charSet='utf-8' />
@@ -18,6 +18,9 @@ const Index = ({ children }) => (
         background-size: cover;
         background-position: center center;
         background-repeat: no-repeat;
+        height: 100%;
+      }
+      #__next {
         height: 100%;
       }
     `}</style>
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,38 @@
 import React from 'react'
 import Body from '~/components/base/Body'
+import Logo from '~/components/base/Logo'
 
-export default () => (
+const logoStyle = {
+  height: 100,
+  width: 100,
+  position: 'fixed',
+  top: 20,
+  left: 20
+}
+
+const wrapperStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  color: 'white',
+  fontSize: 32,
+  height: '100%'
+}
+
+const textStyle = {
+  textAlign: 'center',
+  width: '100%',
+  padding: 40,
+  backgroundColor: 'rgba(0,0,0,0.4)'
+}
+
+const Index = () => (
   <Body>
-    <span> brisa </span>
+    <Logo style={logoStyle} color='white' />
+    <div style={wrapperStyle}>
+      <div style={textStyle}>Estamos enrolando el sitio</div>
+    </div>
   </Body>
 )
+
+export default Index
